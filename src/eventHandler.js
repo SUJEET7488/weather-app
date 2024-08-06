a7c6d57ad26efbb0d54fd423942add9a85a348e3
SUJEET7488 <ksdav19@gmail.com>
2024-08-06 20:22:33 +0530

    few things fixed like url address, add forecast info at bottom

--- src/eventHandler.js
+++ src/eventHandler.js
@@ -1,6 +1,5 @@
 import getWeather from './weatherAPI.js';
 const getWeatherBtn = document.getElementById( 'getWeather' );
-
 const weatherInfo = document.getElementById( 'weatherInfo' );
 const errorMessage = document.getElementById( 'errorMessage' );
 const cityInput = document.getElementById( 'city' );
@@ -14,19 +13,25 @@ const wind = document.getElementById( 'wind' );
 const sunsight = document.getElementById( 'sunsight' );
 const rain = document.getElementById( 'rain' );
 const weatherDiv = document.getElementById( 'weather' );
+const forecast = document.createElement( 'p' );
+forecast.id = 'forecast';
 
 function displayWeather ( weatherData ) {
-    weatherDescription.innerHTML = `Weather: ${ weatherData.weather[ 0 ].main }<br>
-    Weather Description:${ weatherData.weather[ 0 ].description }`;
-    temperature.innerHTML = `Temperature: ${ weatherData.main.temp }°C`;
-    clouds.textContent = `Clouds: ${ weatherData.clouds.all }%`;
+    weatherDescription.innerHTML = `Weather : ${ weatherData.weather[ 0 ].main }<br>
+    Weather Description: ${ weatherData.weather[ 0 ].description }`;
+    temperature.innerHTML = `Temperature : ${ weatherData.main.temp }°C`;
+    clouds.textContent = `Clouds : ${ weatherData.clouds.all }%`;
     humidity.textContent = `Humidity: ${ weatherData.main.humidity }%`;
     wind.textContent = `Wind : ${ weatherData.wind.speed }m/s`;
-    cityName.textContent = `City :${ weatherData.name }, ${ weatherData.sys.country }`;
-    timezone.textContent = `Timezone:${ new Date( weatherData.timezone * 1000 ).toString() }`;
-    sunsight.innerHTML = `Sunrise:${ new Date( weatherData.sys.sunrise * 1000 ).toString() }<br>Sunset:${ new Date( weatherData.sys.sunset * 1000 ).toString() }`;
+    cityName.textContent = `City : ${ weatherData.name }, ${ weatherData.sys.country }`;
+    timezone.textContent = `Timezone : ${ new Date( weatherData.timezone * 1000 ).toString() }`;
+    sunsight.innerHTML = `Sunrise : ${ new Date( weatherData.sys.sunrise * 1000 ).toString() }<br>
+    Sunset : ${ new Date( weatherData.sys.sunset * 1000 ).toString() }`;
+    forecast.textContent = `Forecast : ${ new Date( weatherData.dt * 1000 ).toString() }`;
     rain.textContent = `Rain : ${ weatherData.hasOwnProperty( 'rain' ) ? weatherData.rain[ '1h' ] + 'mm' : 'No Rain' }`
+    weatherInfo.appendChild( forecast );
 }
+
 
 cityInput.addEventListener( 'click', () => {
     weatherDiv.classList.add( 'hidden' );
@@ -36,10 +41,11 @@ getWeatherBtn.addEventListener( 'click', async () => {
     console.log( weatherData );
     if ( weatherData.cod == 200 ) {
         displayWeather( weatherData );
+        
         weatherDiv.contains( errorMessage ) ? weatherDiv.removeChild( errorMessage ) : '';
         weatherDiv.appendChild( weatherInfo );
         weatherDiv.classList.remove( 'hidden' );
-        weatherDiv.style.backgroundImage = `url(./pictures/${ weatherData.weather[ 0 ].main.toLowerCase() }.png)`;
+        weatherDiv.style.backgroundImage = `url(../pictures/${ weatherData.weather[ 0 ].main.toLowerCase() }.png)`;
     }
     if ( weatherData.cod == 404 ) {
         errorMessage.textContent = 'City Not Found';
@@ -51,9 +57,9 @@ getWeatherBtn.addEventListener( 'click', async () => {
 
     if ( weatherData === '' ) {
         errorMessage.textContent = 'Please Enter City Name';
+        weatherDiv.contains( weatherInfo ) ? weatherDiv.removeChild( weatherInfo ) : '';
         weatherDiv.style.removeProperty( "backgroundImage" )
         weatherDiv.appendChild( errorMessage );
-        weatherDiv.contains( weatherInfo ) ? weatherDiv.removeChild( weatherInfo ) : '';
         weatherDiv.classList.remove( 'hidden' );
     }
 } )
@@ -67,7 +73,7 @@ window.addEventListener( 'keydown', async ( e ) => {
         weatherDiv.contains( errorMessage ) ? weatherDiv.removeChild( errorMessage ) : '';
         weatherDiv.appendChild( weatherInfo );
         weatherDiv.classList.remove( 'hidden' );
-        weatherDiv.style.backgroundImage = `url(./pictures/${ weatherData.weather[ 0 ].main.toLowerCase() }.png)`;
+        weatherDiv.style.backgroundImage = `url(../pictures/${ weatherData.weather[ 0 ].main.toLowerCase() }.png)`;
     }
     if ( weatherData.cod == 404 ) {
         errorMessage.textContent = 'City Not Found';
